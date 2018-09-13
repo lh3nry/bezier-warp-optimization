9485bc1cf0bcbd276ce1127106afe6a936c6b192
Henry <henry.s.leigh@gmail.com>
2018-09-12 22:27:15 -0700

    First pass of bilinear interpolation

--- bilinear_interp.m
+++ bilinear_interp.m
@@ -1,4 +1,9 @@
 % bilinear_interp.m
-%% bilinear_interp: bilinear interpolation of a 2D matrix
+%% bilinear_interp: bilinear interpolation of a plane in 3D space
 function [x] = bilinear_interp(A,u,v)
-	x = ;
+	x = zeros(1,3);
+	for i = 1:3
+		G = [A(1,i) A(2,i) ; A(3,i) A(4,i)];
+		x(i) = [1-u u] * G *[1-v ; v];
+	end
+
