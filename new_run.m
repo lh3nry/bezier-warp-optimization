f08eb397aa207cfccd6444a89ee16ae20353142b
Henry Li <henry.s.leigh@gmail.com>
2018-09-13 19:39:33 -0700

    rewrote projection function. started writing tests

--- new_run.m
+++ new_run.m
@@ -7,3 +7,27 @@ clear all;
 preview_cam
 
 scene
+
+im = imread('colour_test.png');
+
+figure(2);
+imshow(im);
+
+
+
+r_height = 10;
+r_width = r_height;
+
+[X,Y] = meshgrid(linspace(1,size(im,1),r_height),linspace(1,size(im,2),r_width));
+
+rays = zeros(r_height*r_width,6);
+
+P = proj_plane(1:4,:);
+for i = 1:r_height
+	for j = 1:r_width
+		rays(i*r_width + j) = projector_ray(o,P,X(j),Y(i),im);
+	end
+end
+
+figure(1);
+
--- projector_ray.m
+++ projector_ray.m
@@ -1,5 +1,7 @@
 % projector_ray.m
 %% projector_ray: generates the rays from the projector origin
-function [color, direction] = projector_ray(p_origin, u,v, image)
-	color = zeros(0,0,0);
-	direction = zeros(0,0,0);
+function [color, direction] = projector_ray(p_origin, proj, x,y, c_image)
+	u = floor(x/size(c_image,1));
+	v = floor(y/size(c_image,2));
+	color = c_image(x,y,:);
+	direction = bilinear_interp(proj,u,v) - p_origin;
