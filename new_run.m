b993ba783a9305fd4669a9156b68e13d2ccb3767
Henry <henry.s.leigh@gmail.com>
2018-09-16 01:13:03 -0700

    Validation of bilinear_interp

--- new_run.m
+++ new_run.m
@@ -15,38 +15,26 @@ imshow(im);
 
 
 
-% r_height = 10;
-% r_width = r_height;
-
-% [X,Y] = meshgrid(linspace(1,size(im,1),r_height),linspace(1,size(im,2),r_width));
-
-% rays = zeros(r_height*r_width,6);
+r_height = 10;
+r_width = r_height;
 
 P = proj_plane(1:4,:);
 
+rays = zeros(8,3);
+rays(1,:) = projector_ray(o,P,0,0,im);
+rays(3,:) = projector_ray(o,P,0,1,im);
+rays(5,:) = projector_ray(o,P,1,0,im);
+rays(7,:) = projector_ray(o,P,1,1,im);
 
-% for i = 1:r_height
-% 	for j = 1:r_width
-% 		% size(rays(i*r_width + j, :))
-% 		% size(projector_ray(o,P,X(j),Y(i),im))
-% 		rays(i*r_width + j, :) = projector_ray(o,P,X(j),Y(i),im);
-% 	end
-% end
-
-rays = zeros(7,6);
-rays(1,:) = projector_ray(o,P,1,1,im)
-rays(2,:) = [o 0 0 0]
-rays(3,:) = projector_ray(o,P,1,1024,im)
-rays(4,:) = [o 0 0 0]
-rays(5,:) = projector_ray(o,P,1024,1,im)
-rays(6,:) = [o 0 0 0]
-rays(7,:) = projector_ray(o,P,1024,1024,im)
-
+rays(2,:) = o;
+rays(4,:) = o;
+rays(6,:) = o;
+rays(8,:) = o;
 
 figure(1);
 hold on;
-% os = repmat(o,size(rays,1),1);
-% quiver3(os(:,1),os(:,2),os(:,3), rays(:,1),rays(:,2),rays(:,3));
+
 plot3(rays(:,1),rays(:,2),rays(:,3),'g');
 plot3(rays([1 3 5 7],1),rays([1 3 5 7],2),rays([1 3 5 7],3),'r*');
+plot3(P(:,1),P(:,2),P(:,3),'b*');
 
--- projector_ray.m
+++ projector_ray.m
@@ -1,10 +1,16 @@
 % projector_ray.m
 %% projector_ray: generates the rays from the projector origin
-function out = projector_ray(p_origin, proj, x,y, c_image)
-	u = x/size(c_image,1);
-	v = y/size(c_image,2);
-	xy = floor([x y]);
-	color_out = reshape(c_image(xy(1),xy(2),:),1,3);
+function out = projector_ray(p_origin, proj, u,v, c_image)
+	% x = u*size(c_image,1);
+	% y = v*size(c_image,2);
+	% xy = floor([x y]);
+	% color_out = reshape(c_image(xy(1),xy(2),:),1,3);
 
-	direction = bilinear_interp(proj,u,v) - p_origin;
-	out = [direction color_out];
+	direction = bilinear_interp(proj,u,v);
+	out = direction;
+
+% %% clamp_pixel_values: function description
+% function [x,y] = clamp_pixel_values(u,v,c_image)
+% 	[n,m,d] = size(c_image);
+% 	x = ;
+% 	y = ;
