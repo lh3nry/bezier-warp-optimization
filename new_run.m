d5b704884b929e76edaa728d2369881d37db714a
Henry Li <henry.s.leigh@gmail.com>
2018-09-15 20:08:12 -0700

    Removed project_image.m. Testing functions using quiver3

--- new_run.m
+++ new_run.m
@@ -23,11 +23,19 @@ r_width = r_height;
 rays = zeros(r_height*r_width,6);
 
 P = proj_plane(1:4,:);
+
+
 for i = 1:r_height
 	for j = 1:r_width
-		rays(i*r_width + j) = projector_ray(o,P,X(j),Y(i),im);
+		% size(rays(i*r_width + j, :))
+		% size(projector_ray(o,P,X(j),Y(i),im))
+		rays(i*r_width + j, :) = projector_ray(o,P,X(j),Y(i),im);
 	end
 end
 
 figure(1);
+hold on;
+os = repmat(o,size(rays,1),1);
+quiver3(os(:,1),os(:,2),os(:,3), rays(:,1),rays(:,2),rays(:,3));
+
 
--- projector_ray.m
+++ projector_ray.m
@@ -1,7 +1,10 @@
 % projector_ray.m
 %% projector_ray: generates the rays from the projector origin
-function [color, direction] = projector_ray(p_origin, proj, x,y, c_image)
+function out = projector_ray(p_origin, proj, x,y, c_image)
 	u = floor(x/size(c_image,1));
 	v = floor(y/size(c_image,2));
-	color = c_image(x,y,:);
-	direction = bilinear_interp(proj,u,v) - p_origin;
+	xy = floor([x y]);
+	color_out = reshape(c_image(xy(1),xy(2),:),1,3);
+
+	direction = bilinear_interp(proj,u,v);
+	out = [direction color_out];
