8ae3481a6b403d052b93a5e7916b4d90a71344c6
lh3nry <henry.s.leigh@gmail.com>
2019-10-27 22:38:44 -0700

    Adding rotation to transforms via SciPy

--- Python/transforms.py
+++ Python/transforms.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 
 def translate(points, x, y, z):
@@ -6,3 +7,8 @@ def translate(points, x, y, z):
         p += np.array([x, y, z])
     return points
 
+
+def rotate(points, axis, theta, degrees):
+    r = R.from_euler(axis, theta, degrees)
+    points = r.apply(points)
+    return points
