9d0e97664328e65bfcecbd115b27dce4b75a1063
lh3nry <henry.s.leigh@gmail.com>
2019-10-31 06:03:11 -0700

    Fix the reshape ordering to match the MATLAB output

--- Python/bezier_patch.py
+++ Python/bezier_patch.py
@@ -30,5 +30,11 @@ def bezier_patch(control_x, control_y, control_z, num_samples):
     r = np.linspace(0, 1, num_samples)
     s = np.linspace(0, 1, num_samples)
 
+    patch_tensor = patch(r, s, control_x, control_y, control_z)
+    print(patch_tensor)
+    print(patch_tensor.shape)
 
+    patch_list = patch_tensor.transpose(2,1,0).reshape(num_samples**2, 3, order='F')
+    print(patch_list)
 
+    return patch_list, patch_tensor
