83aa1749c3194d9fad1215a69c7658ecabaf1f0a
Henry Li <henry.s.leigh@gmail.com>
2019-10-31 03:11:20 -0700

    Construct the anonymous functions used in the bezier patch definition and evaluation.

--- Python/bezier_patch.py
+++ Python/bezier_patch.py
@@ -1,10 +1,29 @@
 def bezier_patch(control_x, control_y, control_z, sample_density):
     import numpy as np
+
+    # print(control_x.shape)
+    # print(control_y.shape)
+    # print(control_z.shape)
+
     basis = [[-1,  3, -3, 1],
              [ 3, -6,  3, 0],
              [-3,  3,  0, 0],
              [ 1,  0,  0, 0]]
     basis = np.array(basis)
-    U = lambda u: [u ** i for i in range(3, -1, -1)]
-    print(U(3))
+    U = lambda u: np.array([u ** i for i in range(3, -1, -1)])
+    W = lambda w: U(w).reshape(-1, 1)
+
+    patch = lambda u,w: np.array([
+        U(u) @ basis @ control_x @ basis @ W(w),
+        U(u) @ basis @ control_y @ basis @ W(w),
+        U(u) @ basis @ control_z @ basis @ W(w)])
+
+    # print(U(0.5))
+    # print(W(0.5))
+    print(U(1) @ basis @ control_x @ basis @ W(1))
+    print(U(0) @ basis @ control_x @ basis @ W(0))
+    print(U(0.5) @ basis @ control_x @ basis @ W(0.5))
+    print(patch(0.5, 0.5))
+    print(patch(0, 0))
+    print(patch(1, 1))
     # return U
