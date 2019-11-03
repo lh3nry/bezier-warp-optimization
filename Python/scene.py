0393f25ab3dbef63bf64f69659e163f36c498e78
lh3nry <henry.s.leigh@gmail.com>
2019-11-03 02:22:48 -0800

    Clean up scene file and add markers for the intersection points

--- Python/scene.py
+++ Python/scene.py
@@ -1,6 +1,5 @@
 import numpy as np
 import transforms as trfs
-from matplotlib.colors import ListedColormap as ListCMAP
 import plotly.graph_objs as go
 import bezier_patch as bpatch
 
@@ -23,7 +22,6 @@ def plane_bilinear(corners, u, v):
     V = np.array([[1-v], [v]])
 
     interpolated = (G @ V).reshape(2, 3, order='F')
-    # interpolated = U @ interpolated
 
     return U @ interpolated
 
@@ -44,15 +42,10 @@ view_plane = trfs.translate(view_plane, 0, 25, 0)
 
 viewpoint = view_plane[4]
 
-# print(viewpoint)
-
 upper_left = view_plane[2]
 upper_right = view_plane[0]
 lower_right = view_plane[1]
 lower_left = view_plane[3]
-
-cmap_name = 'white'
-cmap_white = ListCMAP([(1, 1, .5)], cmap_name, 1)
 
 w_proj = 3
 distance = 6
@@ -75,31 +68,12 @@ proj_plane = trfs.translate(proj_plane, 0, 9, -17)
 proj_origin = proj_plane[4]
 
 X, Y, Z = unpack_array_to_tuple(view_plane[:4])
-I, J, K = unpack_array_to_tuple(view_plane_triangulated)
 mesh1 = go.Mesh3d(x=X, y=Y, z=Z,
                   delaunayaxis='y',
                   color='cyan')
 A, B, C = unpack_array_to_tuple(proj_plane[:4])
 mesh2 = go.Mesh3d(x=A, y=B, z=C, color='lightpink')
-
 figure_data = [mesh1, mesh2]
-
-test = go.Scatter3d(x=X, y=Y, z=Z, mode='markers')
-
-layout = go.Layout(
-    width=1024,
-    height=1024,
-    scene=dict(
-        camera=dict(
-            eye=dict(x=1.15, y=1.15, z=0.8)),  # the default values are 1.25, 1.25, 1.25
-        xaxis=dict(),
-        yaxis=dict(),
-        zaxis=dict(),
-        aspectmode='manual',  # this string can be 'data', 'cube', 'auto', 'manual'
-        # a custom aspectratio is defined as follows:
-        aspectratio=dict(x=.85, y=1, z=1)
-    )
-)
 
 Cx = [[-15, -15, -15, -15],
       [ -5,  -5,  -5,  -5],
@@ -124,32 +98,18 @@ Q, T, patch_tri = bpatch.bezier_patch(Cx, Cy, Cz, 15)
 Px, Py, Pz = unpack_array_to_tuple(Q)
 I, J, K = unpack_array_to_tuple(np.array(patch_tri))
 mesh3 = go.Mesh3d(x=Px, y=Py, z=Pz, i=I, j=J, k=K)
-bez_scatter = go.Scatter3d(x=Px, y=Py, z=Pz, mode='markers')
-
+# bez_scatter = go.Scatter3d(x=Px, y=Py, z=Pz, mode='markers')
 figure_data.append(mesh3)
-
-# print(view_plane[:4])
-# print(plane_bilinear(view_plane[:4], 1, 1))
-# print(plane_bilinear(view_plane[:4], 0, 0))
-# print(plane_bilinear(view_plane[:4], 0, 1))
-# print(plane_bilinear(view_plane[:4], 1, 0))
 
 ray_density = 5
 ray_edge = np.linspace(0, 1, ray_density)
 x_rays, y_rays = np.meshgrid(ray_edge, ray_edge)
-# print(x_rays)
-# print(y_rays)
-
 rays = np.stack((x_rays, y_rays))
-# print(rays)
 
 rays = rays.transpose((2, 1, 0)).reshape(x_rays.size, 2, order='F')
-# for ray in rays:
-#     print(plane_bilinear(view_plane[:4], ray[0], ray[1]))
 
 ray_points = np.array(
     [plane_bilinear(proj_plane[:4], ray[0], ray[1]) for ray in rays])
-# print(ray_points)
 
 X, Y, Z = unpack_array_to_tuple(np.array(view_plane))
 view_points = go.Scatter3d(x=X, y=Y, z=Z, mode='markers')
@@ -160,27 +120,30 @@ proj_points = go.Scatter3d(x=X, y=Y, z=Z, mode='markers')
 figure_data.append(proj_points)
 figure_data.append(view_points)
 
-# intersect = bpatch.intersect(Cx, Cy, Cz, proj_origin, ray_points[0])
-# ray_i = np.concatenate((intersect, proj_origin[None,:]), axis=0)
-# X, Y, Z = unpack_array_to_tuple(np.array(ray_i))
-# ray_plot = go.Scatter3d(x=X, y=Y, z=Z, mode='lines', line=dict(width=2))
-# figure_data.append(ray_plot)
-
-
 for point in ray_points:
     intersect = bpatch.intersect(Cx, Cy, Cz, proj_origin, point)
-    # print(intersect)
-    # print(intersect.shape)
-    # print(proj_origin)
-    # print(proj_origin.shape)
-    # ray_i = np.array([point, proj_origin])
-    # ray_i = np.array([bpatch.intersect(Cx, Cy, Cz, proj_origin, point), proj_origin])
     ray_i = np.concatenate((intersect, proj_origin[None,:]), axis=0)
-    # print(ray_i)
     X, Y, Z = unpack_array_to_tuple(np.array(ray_i))
-    ray_plot = go.Scatter3d(x=X, y=Y, z=Z, mode='lines', line=dict(width=2))
-    # ray_plot = go.Scatter3d(x=X, y=Y, z=Z, mode='markers')
+    ray_plot = go.Scatter3d(x=X, y=Y, z=Z, mode='lines', line=dict(color='lightpink', width=2), showlegend=False)
     figure_data.append(ray_plot)
+    X, Y, Z = unpack_array_to_tuple(intersect)
+    intersection_markers = go.Scatter3d(x=X, y=Y, z=Z, 
+                                        mode='markers', marker=dict( color='aliceblue', size=3), showlegend=False)
+    figure_data.append(intersection_markers)
 
+layout = go.Layout(
+    width=1024,
+    height=1024,
+    scene=dict(
+        camera=dict(
+            eye=dict(x=1.15, y=1.15, z=0.8)),  # the default values are 1.25, 1.25, 1.25
+        xaxis=dict(),
+        yaxis=dict(),
+        zaxis=dict(),
+        aspectmode='manual',  # this string can be 'data', 'cube', 'auto', 'manual'
+        # a custom aspectratio is defined as follows:
+        aspectratio=dict(x=.85, y=1, z=1)
+    )
+)
 fig = go.Figure(data=figure_data, layout=layout)
 fig.show()
