11665fdad6a9d4ec6febd8be97a458499bf32128
lh3nry <henry.s.leigh@gmail.com>
2019-11-30 04:19:36 -0800

    Move intersection plotting in new_run to a subroutine as in plotly_anim_trial.py

--- Python/new_run.py
+++ Python/new_run.py
@@ -3,6 +3,27 @@ from scene import *
 # import numpy as np
 # import utils as u
 
+def intersection_test(direction, origin):
+    intersect, U, V = bpatch.intersect(Cx, Cy, Cz, origin, direction)
+    # assert(0 <= U <= 1)
+    # assert(0 <= V <= 1)
+
+    if 0 <= U <= 1 and 0 <= V <= 1:
+        ray_i = np.concatenate((intersect, origin[None, :]), axis=0)
+        X, Y, Z = u.unpack_array_to_tuple(np.array(ray_i))
+        ray_plot = go.Scatter3d(
+            x=X, y=Y, z=Z,
+            mode='lines',
+            line=dict(color='lightpink', width=2),
+            showlegend=False)
+        figure_data.append(ray_plot)
+        X, Y, Z = u.unpack_array_to_tuple(intersect)
+        intersection_markers = go.Scatter3d(
+            x=X, y=Y, z=Z,
+            mode='markers',
+            marker=dict(color='aliceblue', size=3),
+            showlegend=False)
+        figure_data.append(intersection_markers)
 
 ray_density = 5
 ray_edge = np.linspace(0, 1, ray_density)
@@ -15,22 +36,7 @@ ray_points = np.array(
     [u.bilinear_sample_plane(proj_plane[:4], ray[0], ray[1]) for ray in rays])
 
 for point in ray_points:
-    intersect = bpatch.intersect(Cx, Cy, Cz, proj_origin, point)
-    ray_i = np.concatenate((intersect, proj_origin[None,:]), axis=0)
-    X, Y, Z = u.unpack_array_to_tuple(np.array(ray_i))
-    ray_plot = go.Scatter3d(
-        x=X, y=Y, z=Z,
-        mode='lines',
-        line=dict(color='lightpink', width=2),
-        showlegend=False)
-    figure_data.append(ray_plot)
-    X, Y, Z = u.unpack_array_to_tuple(intersect)
-    intersection_markers = go.Scatter3d(
-        x=X, y=Y, z=Z,
-        mode='markers',
-        marker=dict( color='aliceblue', size=3),
-        showlegend=False)
-    figure_data.append(intersection_markers)
+    intersection_test(point, proj_origin)
 
 fig = go.Figure(data=figure_data, layout=layout)
 fig.show()
